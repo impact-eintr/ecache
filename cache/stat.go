44ce6b50f4c1fced6f99f0390577a713d5598225
impact-eintr <yixingwei4@gmail.com>
2021-07-23 15:56:31 +0800

    ecached status

--- cache/stat.go
+++ cache/stat.go
@@ -1,7 +1,5 @@
 package cache
 
-import "log"
-
 type Stat struct {
 	Count     int64 `json:"count"`
 	KeySize   int64 `json:"keysize"`
@@ -12,12 +10,10 @@ func (s *Stat) Add(k string, v []byte) {
 	s.Count += 1
 	s.KeySize += int64(len(k))
 	s.ValueSize += int64(len(v))
-	log.Println("add()", s)
 }
 
 func (s *Stat) Remove(k string, v []byte) {
 	s.Count -= 1
 	s.KeySize -= int64(len(k))
 	s.ValueSize -= int64(len(v))
-	log.Println("remove()", s)
 }
